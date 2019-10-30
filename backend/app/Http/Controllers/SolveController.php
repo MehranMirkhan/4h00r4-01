e3678609a85692044cd1503e22a75ee859ae5e8e
MehranMirkhan <mehranmirkhan@gmail.com>
2019-10-30 19:19:42 +0330

    solves

--- backend/app/Http/Controllers/SolveController.php
+++ backend/app/Http/Controllers/SolveController.php
@@ -6,72 +6,30 @@ use App\Models\Solve;
 use Illuminate\Http\Request;
 
 class SolveController extends Controller {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index() {
-        //
+        return Solve::all();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create() {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request) {
-        //
+        $solve = Solve::create($request->all());
+        return Solve::query()->where('id', $solve->id)->firstOrFail();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param \App\Models\Solve $solve
-     * @return \Illuminate\Http\Response
-     */
     public function show(Solve $solve) {
-        //
+        return $solve;
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param \App\Models\Solve $solve
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Solve $solve) {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Models\Solve $solve
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Solve $solve) {
-        //
+        $solve->update($request->all());
+        return Solve::query()->where('id', $solve->id)->firstOrFail();
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Models\Solve $solve
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Solve $solve) {
-        //
+        try {
+            $solve->delete();
+            return response()->json('پاسخ حذف شد', 200);
+        } catch (\Exception $e) {
+            return response()->json('خطای نامشخص', $e->getCode());
+        }
     }
 }
