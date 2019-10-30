18f4531bc38850c827a279d3cb08ba5dda9c1015
MehranMirkhan <mehranmirkhan@gmail.com>
2019-10-30 19:20:17 +0330

    answers

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,7 @@ Route::group(['middleware' => ['auth:api', 'scope:admin'], 'prefix' => 'admin'],
         Route::apiResource('users', 'UserController')->except(['store']);
         Route::apiResource('questions', 'QuestionController');
         Route::apiResource('solutions', 'SolutionController');
+        Route::apiResource('answers', 'AnswerController');
     });
 });
 
