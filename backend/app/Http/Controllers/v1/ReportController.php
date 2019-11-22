19f97a40719db6d083293211b42fc801b2225f88
MehranMirkhan <mehranmirkhan@gmail.com>
2019-11-22 15:57:06 +0330

    report

--- backend/app/Http/Controllers/v1/ReportController.php
+++ backend/app/Http/Controllers/v1/ReportController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers\v1;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\Question;
+use Carbon\Carbon;
 
 
 class ReportController extends Controller {
     public function index() {
         $report = [
+            "server" => [
+                "timestamp" => Carbon::now(),
+            ],
             "users" => [
                 "count" => User::count(),
             ],
--- frontend-admin/src/modules/report/index.jsx
+++ frontend-admin/src/modules/report/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Segment } from 'semantic-ui-react';
+import { Segment, Grid, Statistic } from 'semantic-ui-react';
 
 import { fetchReport } from './report.reducer';
 
@@ -13,13 +13,40 @@ class Report extends React.Component {
   }
   render() {
     const { report } = this.props;
+    console.log("Report:", report);
     return <Layout>
       <Segment raised textAlign="center" color="blue" inverted style={{ margin: '16px 0' }}>
         <h1>گزارش</h1>
       </Segment>
-      <Segment compact>
-        {JSON.stringify(report)}
-      </Segment>
+      {report.server &&
+        <Grid columns={3}>
+          <Grid.Column>
+            <Segment textAlign="center">
+              <h2>سرور</h2>
+              <div style={{ height: 12 }}/>
+              زمان: {(new Date(report.server.timestamp)).toUTCString()}
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment textAlign="center">
+              <h2>کاربران</h2>
+              <Statistic>
+                <Statistic.Label>تعداد کاربران</Statistic.Label>
+                <Statistic.Value>{report.users.count}</Statistic.Value>
+              </Statistic>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment textAlign="center">
+              <h2>سؤالات</h2>
+              <Statistic>
+                <Statistic.Label>تعداد سؤالات</Statistic.Label>
+                <Statistic.Value>{report.questions.count}</Statistic.Value>
+              </Statistic>
+            </Segment>
+          </Grid.Column>
+        </Grid>
+      }
     </Layout>;
   }
 }
