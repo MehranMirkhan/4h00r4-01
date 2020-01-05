ac9dab12b6cc77cdc24cc48ca0bf2536d0a51f1b
MehranMirkhan <mehranmirkhan@gmail.com>
2020-01-05 10:37:43 +0330

    front admin build

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/{any}', function ($any) {
+    return view('index');
+})->where('any', '.*');
