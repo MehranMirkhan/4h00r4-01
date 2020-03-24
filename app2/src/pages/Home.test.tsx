001338a9139dd546d747ae894f411bcce476bea1
MehranMirkhan <mehranmirkhan@gmail.com>
2020-03-24 16:04:25 +0430

    Home page

--- app2/src/pages/Home.test.tsx
+++ app2/src/pages/Home.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import { Home } from ".";
+import { Home } from "./Home";
 
 describe("<Home />", () => {
   it("should render", () => {
